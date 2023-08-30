d39256ba1330855ed562d5f8e5887292d9660cef
Krishna <vamsirk99@gmail.com>
2023-08-30 12:21:55 -0500

    03-basics aws-backend final

--- 03-basics/aws_backend/main.tf
+++ 03-basics/aws_backend/main.tf
@@ -1,11 +1,11 @@
 terraform{
-    # backend "s3" {
-    #   bucket = "terraform-demo-tf-state"
-    #   key = "03-basics/import-bootstrap/terraform.tfstate"
-    #   region = "us-east-1"
-    #   dynamodb_table = "terraform-state-locking"
-    #   encrypt = true
-    # }
+    backend "s3" {
+      bucket = "terraform-demo-tf-state"
+      key = "03-basics/import-bootstrap/terraform.tfstate"
+      region = "us-east-1"
+      dynamodb_table = "terraform-state-locking"
+      encrypt = true
+    }
     required_providers {
         aws = {
             source = "hashicorp/aws"
